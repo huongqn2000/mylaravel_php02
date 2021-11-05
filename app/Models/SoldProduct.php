a37098fd5727ad3bb3facc48cea9c9e03a23095e
huongqn2000 <60293303+huongqn2000@users.noreply.github.com>
2021-11-05 17:52:29 +0700

    commit

--- app/Models/SoldProduct.php
+++ app/Models/SoldProduct.php
@@ -5,22 +5,23 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class ReceiptProduct extends Model
+class SoldProduct extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'receipt_id',
+        'sale_id',
         'product_id',
-        'stock',
-        'stock_defective'
+        'qty',
+        'price',
+        'total_amount'
     ];
-
-    public function receipt(){
-        return $this->belongsTo(Receipt::class, 'receipt_id', 'id');
-    }
 
     public function product(){
         return $this->belongsTo(Product::class, 'product_id', 'id');
+    }
+
+    public function sale(){
+        return $this->belongsTo(Sale::class, 'sale_id', 'id');
     }
 }
