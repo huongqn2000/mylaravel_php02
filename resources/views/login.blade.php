e536b8e3a59cf45e92fc565dc9fd7f419708e78e
huongqn2000 <60293303+huongqn2000@users.noreply.github.com>
2021-11-07 23:24:20 +0700

    commit

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,1 +1,32 @@
-<?php
+<main>
+
+    <form action="login-process.php" method="post" style="width: 50%; margin: 50px 0 50px 50px;">
+        <div class="row mb-3">
+            <label for="txtEmail" class="col-sm-2 col-form-label">Email</label>
+            <div class="col-sm-10">
+                <input type="email" name="txtEmail"
+                       class="form-control <?php if(isset($_SESSION['errors']['txtEmail'])):?>is-invalid<?php endif?>"
+                       id="txtEmail" aria-describedby="emailHelp" >
+                <?php if(isset($_SESSION['errors']['txtEmail'])):?>
+<div class="invalid-feedback">
+    <?php echo $_SESSION['errors']['txtEmail'];?>
+</div>
+<?php endif?>
+</div>
+</div>
+<div class="row mb-3">
+    <label for="txtPass" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-10">
+        <input type="password" name="txtPass"
+               class="form-control <?php if(isset($_SESSION['errors']['txtPass'])):?>is-invalid<?php endif?>"
+               id="txtPass" >
+        <?php if(isset($_SESSION['errors']['txtPass'])):?>
+        <div class="invalid-feedback">
+            <?php echo $_SESSION['errors']['txtPass'];?>
+        </div>
+        <?php endif?>
+    </div>
+</div>
+<button type="submit" class="btn btn-primary">Login</button>
+</form>
+</main>
