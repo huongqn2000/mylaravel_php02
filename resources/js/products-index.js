28c19da609451cb1d8970d8f8df65367d5f9a80e
huongqn2000 <60293303+huongqn2000@users.noreply.github.com>
2021-11-29 15:37:30 +0700

    CRUD

---
+++ resources/js/products-index.js
@@ -0,0 +1,28 @@
+$(function() {
+    var myModal = new bootstrap.Modal(document.getElementById('staticBackdrop'), {
+        keyboard: false
+    });
+
+    $("#table").on("click", ".delete", function (){
+        var id = $(this).attr('data-id');
+        $('#f_delete').attr('action', '/ims/products/' + id + '/delete');
+        myModal.toggle();
+    });
+
+    var table = $('#table').DataTable({
+        processing: true,
+        serverSide: true,
+        ajax: products_data_url,
+        "order": [[ 0, "desc" ]],
+        "autoWidth": true,
+        columns: [
+            { data: 'id', name: 'id' },
+            { data: 'name', name: 'name'},
+            { data: 'description', name: 'description'},
+            { data: 'price', name: 'price'},
+            { data: 'created_at', name:'created_at'},
+            { data: 'updated_at', name:'updated_at'},
+            { data: 'actions', name: 'actions', orderable: false, searchable: false }
+        ]
+    });
+});
